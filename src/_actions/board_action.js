9717adb90eed26e24b222946b6c2f8ea9141aaba
Jin Woo Jang <jjw213>
2022-05-18 15:08:55 +0900

    dog&cat

--- src/_actions/board_action.js
+++ src/_actions/board_action.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 import {
-  SAVE_POST,
-
+    SAVE_POST,
+    LOAD_ANIMAL
 } from './types';
 // import { USER_SERVER } from '../components/Config.js';
 
@@ -15,6 +15,17 @@ export async function savePost(dataToSubmit){
         //.then(setAuthHeader(response => response.dataToSubmit.token));
     return {
         type: SAVE_POST,
+        payload: request
+    }
+}
+export async function loadAnimal(dataToSubmit){
+    const request = await axios.post(`/animal/animalList`,null, 
+    {params:{numOfRows : dataToSubmit.numOfRows, kindcd : dataToSubmit.kindcd
+    }})
+        .then(response => response.data)
+        //.then(setAuthHeader(response => response.dataToSubmit.token));
+    return {
+        type: LOAD_ANIMAL,
         payload: request
     }
 }
--- src/view/ApiTest.js
+++ src/view/ApiTest.js
@@ -1,39 +1,67 @@
 import axios from 'axios';
 import React,{ useEffect, useState } from 'react'
-
+import {loadAnimal} from "../_actions/board_action"
+import { useDispatch } from "react-redux";
 function ApiTest() {
     const [animal, setAnimal] = useState([]);
-    const [Num, setNum] = useState("")
-    const onNumHandeler = (event) => {
-      setNum(event.currentTarget.value)
+    const [kindcd, setKindcd] = useState("")
+    const dispatch = useDispatch();
+    const onDogHandler = (event) => {
+      event.preventDefault();
+      console.log(event.currentTarget.value)
+      setKindcd(event.currentTarget.value)
+    let body = {
+      numOfRows: 30,
+      kindcd: "개",
+    };
+    console.log(body.kindcd);
+      dispatch(loadAnimal(body))
+      .then((response) => {
+        console.log(response.payload);
+        if (response.payload != null) {
+          setAnimal(response.payload);          
+        } else {
+          alert("아이디 혹은 비번 틀림");
+        }
+      })
     }
-    const endPoint = async()=>{
-        axios.post(`/animal/animalList`,null, 
-        {params:{numOfRows : Num }})
-        // .then(res=>res.json())
-        // .then(res=>console.log(res.data))
-        .then(res=>setAnimal(res.data))
-        //  console.log(res);
-        console.log(animal)
+    const onCatHandler = (event) => {
+      console.log(event.currentTarget.value)
+      setKindcd(event.currentTarget.value)
+      console.log(kindcd);
     }
-    useEffect(()=>{
-      if(Num)
-        endPoint();
-    },[Num])
+
+
+    // const endPoint = async()=>{
+    //     axios.post(`/animal/aSelectList`,null, 
+    //     {params:{numOfRows : 30, kindcd: kindcd }})
+    //     // .then(res=>res.json())
+    //     // .then(res=>console.log(res.data))
+    //     .then(res=> {setAnimal(res.data)
+    //     console.log(res.data)})
+    //     //  console.log(res);
+    //     console.log(animal);
+    // }
+    // useEffect(()=>{
+    //   if(kindcd !=null && kindcd != "")
+    //     endPoint();
+    // },[kindcd])
   return (
     <div>
       <form>
-      <label htmlFor="name">페이지 수</label>
-                <input type="text" id="name" name="numOfRows" onChange={onNumHandeler} placeholder="이름을 입력하세요"></input>
-                {/* <button onSubmit={onSubmitHandler}  >로그인</button> */}
+      <label htmlFor="name">개 / 고양이</label>
+                {/* <input type="text" id="name" name="numOfRows" onChange={onNumHandeler} placeholder="이름을 입력하세요"></input> */}
       </form>
+                <button onClick={onCatHandler} value="고양이" >고양이</button>
+                <button onClick={onDogHandler} value="개" >강아지</button>
       {animal && animal.map(ele =>
             <div key={ele.desertionNo}>
               <h2>종 : {ele.kindCd}</h2>
               <div>나이 : {ele.age}</div>
               <div>특징 : {ele.specialMark}</div>
+              <div><img src={ele.popfile}></img></div>
             </div>)}
-      id : {animal && animal.desertionNo}
+
     </div>
   )
 }
