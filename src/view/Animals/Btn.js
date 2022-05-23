482c273938e696994126274b96586219132548dc
yeyae <whyjinny@naver.com>
2022-05-23 10:02:27 +0900

    btn

--- src/view/Animals/Btn.js
+++ src/view/Animals/Btn.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import catBtn1 from "../../css/img/catIcon1.png";
 import dogBtn1 from "../../css/img/dogIcon1.png";
 import etcBtn1 from "../../css/img/etcIcon1.png";
@@ -8,62 +8,61 @@ import etcBtn2 from "../../css/img/etcIcon2.png";
 import "../../css/ApiTest.css";
 
 function Btn(props) {
-    const {dog, cat, animals, isHover1, isHover2, isHover3, submit} = props;
+  const { dog, cat, animals, isHover1, isHover2, isHover3, submit } = props;
   return (
-      
     <div className="animalBtn">
-        <div className="searchName">
-            <label htmlFor="name">👀 찾고자 하는 동물을 선택해주세요 👀</label>
-        </div>
-        <button
-          onClick={cat}
-          value="고양이"
-          className="catBtn"
-          style={{
-            border: "none",
-            width: "80px",
-            backgroundColor: "white",
-            cursor: "pointer",
-          }}
-        >
-          <img
-            src={isHover1 ? catBtn2 : catBtn1}
-            style={{ width: "100%", height: "100%" }}
-          />
-        </button>
-        <button
-          onClick={dog}
-          value="개"
-          className="dogBtn"
-          style={{
-            border: "none",
-            width: "80px",
-            backgroundColor: "white",
-            cursor: "pointer",
-          }}
-        >
-          <img
-            src={isHover2 ? dogBtn2 : dogBtn1}
-            style={{ width: "100%", height: "100%" }}
-          />
-        </button>
-        <button
-          onClick={animals}
-          value="기타"
-          className="dogBtn"
-          style={{
-            border: "none",
-            width: "80px",
-            backgroundColor: "white",
-            cursor: "pointer",
-          }}
-        >
-          <img
-            src={isHover3 ? etcBtn2 : etcBtn1}
-            style={{ width: "100%", height: "100%" }}
-          />
-        </button>
-        <div className="submitCenter">
+      <div className="searchName">
+        <label htmlFor="name">👀 찾고자 하는 동물을 선택해주세요 👀</label>
+      </div>
+      <button
+        onClick={cat}
+        value="고양이"
+        className="catBtn"
+        style={{
+          border: "none",
+          width: "80px",
+          backgroundColor: "white",
+          cursor: "pointer",
+        }}
+      >
+        <img
+          src={isHover1 ? catBtn2 : catBtn1}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </button>
+      <button
+        onClick={dog}
+        value="개"
+        className="dogBtn"
+        style={{
+          border: "none",
+          width: "80px",
+          backgroundColor: "white",
+          cursor: "pointer",
+        }}
+      >
+        <img
+          src={isHover2 ? dogBtn2 : dogBtn1}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </button>
+      <button
+        onClick={animals}
+        value="기타"
+        className="dogBtn"
+        style={{
+          border: "none",
+          width: "80px",
+          backgroundColor: "white",
+          cursor: "pointer",
+        }}
+      >
+        <img
+          src={isHover3 ? etcBtn2 : etcBtn1}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </button>
+      <div className="submitCenter">
         <button
           className="submitBtn submitBtnPush"
           onClick={submit}
@@ -77,8 +76,8 @@ function Btn(props) {
           찾기
         </button>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Btn
+export default Btn;
