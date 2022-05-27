87ca215a1dd1a5b0bc3c1df5dcf00773449cc265
lghg <lghg@naver.com>
2022-05-27 13:21:44 +0900

    css

--- src/view/Mainpage/Slide.js
+++ src/view/Mainpage/Slide.js
@@ -4,11 +4,20 @@ import axios from "axios";
 function Slide(){
 
     const [animal, setAnimal] = useState([]);
+    const [img1, setImg1] = useState([]);
+    const [img2, setImg2] = useState([]);
+    const [img3, setImg3] = useState([]);
+    const [img4, setImg4] = useState([]);
 
     const endPoint = async()=>{
         axios.post(`/animal/animalList`,null, 
         {params:{numOfRows : 4 , kindcd:"개",upr_cd:6110000}})
-        .then(res=>setAnimal(res.data))
+        .then(res=>{setAnimal(res.data)
+        setImg1(res.data[0].popfile)
+        setImg2(res.data[1].popfile)
+        setImg3(res.data[2].popfile)
+        setImg4(res.data[3].popfile)
+        })
         console.log(animal)
     }
     useEffect(()=>{
@@ -27,12 +36,28 @@ function Slide(){
             <div className="slidelist">
             
                 <div className="slideitem">
-                    {animal && animal.map(ele =>
+                    {/* {animal && animal.map(ele =>
                         <div className="Float" key={ele.desertionNo}>
                             <img src={ele.popfile}/>
                         </div>)
-                    }
+                    } */}
+                    <div className="Float" >
+                            <img src={img1}/>
+                        </div>
+                    <div className="Float" >
+                            <img src={img2}/>
+                        </div>
+                    <div className="Float" >
+                            <img src={img3}/>
+                        </div>
+                    <div className="Float" >
+                            <img src={img4}/>
+                        </div>
+                        <div className="Float" >
+                            <img src={img1}/>
+                        </div>
                 </div>
+
 
             </div>
             
