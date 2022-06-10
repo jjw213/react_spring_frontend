90b9ed8b6a593a8ffedd43100fbcee60129f4579
lghg <lghg@naver.com>
2022-06-10 16:01:34 +0900

    button2

--- src/view/Mypage/MyPage.js
+++ src/view/Mypage/MyPage.js
@@ -115,7 +115,7 @@ function MyPage() {
             <section className="UserName">회원이름 : {user.userData} </section>
 
             <section className="PassID">
-                <button onClick={openModal2}>회원인증</button>
+                <button className="w-btn w-btn-gra1" onClick={openModal2}>회원인증</button>
             </section>
             <Modal2
                 open={modalOpen2}
@@ -133,7 +133,7 @@ function MyPage() {
 
 
             <section className="DeleteID">
-                <button onClick={openModal}> 탈퇴 버튼 </button>
+                <button className="w-btn w-btn-gra1" onClick={openModal}> 탈퇴 버튼 </button>
             </section>
             <Modal
                 open={modalOpen}
